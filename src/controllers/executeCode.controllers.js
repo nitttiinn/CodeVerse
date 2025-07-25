437f74e936ef571cbb50352236a58654c792636a
nittin <nittinsy12@gmail.com>
2025-07-25 07:31:33 +0530

    feat(code-execution): add executeCode controller with test case validation and batch Judge0 integration

--- src/controllers/executeCode.controllers.js
+++ src/controllers/executeCode.controllers.js
@@ -1,8 +1,7 @@
-import { SubmitBatch } from "../libs/judge0.lib.js";
+import { pollBatchResults, SubmitBatch } from "../libs/judge0.lib.js";
 
 export const executeCode = async(req, res) =>{
     const userId = req.user.id;
-
     if(!userId){
         return res.status(404).json({
             success: false,
@@ -10,9 +9,8 @@ export const executeCode = async(req, res) =>{
         });
     };
 
-    const {source_code, language_id, stdin, expected_ouputs, problemId} = req.body;
-        
-    if(!source_code || !language_id || !stdin || !expected_ouputs || !problemId){
+    const {source_code, language_id, stdin, expected_outputs, problemId} = req.body;
+    if(!source_code || !language_id || !stdin || !expected_outputs || !problemId){
         return res.status(400).json({
             success: false,
             error: 'Bad Request - All fields are required.'
@@ -20,7 +18,7 @@ export const executeCode = async(req, res) =>{
     };
 
     // 1. validate test cases
-    if(!Array.isArray(stdin) || stdin.length === 0 || !Array.isArray(expected_ouputs) || expected_ouputs.length !== stdin.length){
+    if(!Array.isArray(stdin) || stdin.length === 0 || !Array.isArray(expected_outputs) || expected_outputs.length !== stdin.length){
         return res.status(400).json({
             success: false,
             error: 'Bad Request - Invalid or missing test cases!'
@@ -32,17 +30,28 @@ export const executeCode = async(req, res) =>{
         const submissions = stdin.map((input) =>({
             source_code,
             language_id,
-            stdin: input,
-            base64_encoded: false,
-            wait: false
+            stdin: input
         }));
 
         // 3. submit the batch to judge0
         const submitResponse  = await SubmitBatch(submissions);
+        const tokens = submitResponse.map((res) => res.token);
 
+        // 4. Poll Judge0 for reusults of all submitted test cases
+        const result = await pollBatchResults(tokens);
+        console.log("Result-----------");
+        console.log(result);
+
+        res.status(200).json({
+            success: true,
+            message: "Code Executed Successfully!"
+        });
         
     } catch (error) {
         console.log("Error During Executing Code: ", error);
-        
-    }
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error - Unable to execute code."
+        });
+    };
 }
